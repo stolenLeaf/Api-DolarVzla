db009e5c8b11e6b6a80fa593f007e7df04763b0c
Alexander <alexander.arguello@hotmail.com>
2022-12-19 13:31:21 -0400

    cord added

--- build/index.js
+++ build/index.js
@@ -5,8 +5,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const routes_1 = __importDefault(require("./routes"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT || 3001;
+app.use((0, cors_1.default)());
 app.use(express_1.default.json()); //middleweware que se encarga de convertir la req.body en unJSON
 app.use('/api', routes_1.default);
 app.use('/', (_req, res) => {
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import express from 'express'
 import api from './routes'
+import cors from 'cors'
+
 const app=express()
 const PORT = process.env.PORT || 3001
+app.use(cors())
 app.use(express.json())//middleweware que se encarga de convertir la req.body en unJSON
 app.use('/api',api)
-
-
 app.use('/', (_req, res) => {
     res.json({
       mensaje: 'hola mundo'
